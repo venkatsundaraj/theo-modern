5714a3798130448584503402d4953661876f131e
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-06-06 20:57:11 +0530

    intro

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const allLists = await api.example.hello({ text: "Venkat" });
+  console.log(allLists);
+  // const session = await getServerAuthSession();
+
+  return <h1>hello world</h1>;
+}
+
+// async function CrudShowcase() {
+//   const session = await getServerAuthSession();
+//   if (!session?.user) return null;
+
+//   const latestPost = await api.post.getLatest();
+
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
+
+//       <CreatePost />
+//     </div>
+//   );
+// }
---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return `Damn You are right, ${input.text}`;
+    }),
+});
