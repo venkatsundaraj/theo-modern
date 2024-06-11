2a3bcebe9010dbd33b1c6027fa266a7634f439d9
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-06-11 15:58:25 +0530

    dynamic + images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,32 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+
+const coreConfig = {
+  images: {
+    remotePatterns: [{ hostname: "utfs.io" }],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+};
+
+const config = coreConfig;
 
 export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,31 @@ import Link from "next/link";
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
+import Image from "next/image";
 
+export const dynamic = "force-dynamic";
+
+const IMAGE_DATA = [
+  "https://utfs.io/f/a1a81bfe-e387-47ef-80f4-9cb7d276de19-2a0eht.png",
+  "https://utfs.io/f/b01a0f6a-88a4-4a4c-b96c-7d6be62cea63-xndk9k.png",
+  "https://utfs.io/f/e4e6f729-6e27-488c-818e-736b6b5ada6c-8u23eg.png",
+  "https://utfs.io/f/8ee0230e-0f2a-4393-8a00-f53cff175c0b-u57gro.png",
+  "https://utfs.io/f/d305773d-445e-49b8-aabc-1837ae7a4e5a-j8diko.png",
+];
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const allLists = await api.example.hello({ text: "Venkat" });
   console.log(allLists);
-  // const session = await getServerAuthSession();
 
-  return <h1>hello world</h1>;
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      {IMAGE_DATA.map((item, i) => {
+        return (
+          <Image width={400} height={200} src={item} alt={"One"} key={i} />
+        );
+      })}
+    </div>
+  );
 }
 
 // async function CrudShowcase() {
